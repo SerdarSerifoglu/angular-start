d6b11971d0056c894e91f0f493b8801892bb0cd0
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-10-29 07:12:21 +0300

    *ngFor ile ürünler listelendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  products = [
+    { id: 1, name: "Laptop", categoryId: 1, unitPrice: 40 },
+    { id: 2, name: "Masa", categoryId: 1, unitPrice: 2 },
+    { id: 3, name: "Sandalye", categoryId: 1, unitPrice: 1 },
+    { id: 4, name: "Lamba", categoryId: 1, unitPrice: 1 }
+  ]
   constructor() { }
 
   ngOnInit(): void {
