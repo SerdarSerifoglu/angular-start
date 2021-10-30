301e9af748ed6124c896304a2b89fb0576e15d17
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-10-30 13:23:27 +0300

    HttpClient eklendi ve örnek request yollandı

--- src/app/components/pack/pack.component.ts
+++ src/app/components/pack/pack.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Pack } from 'src/app/models/pack';
+import { PackResponseModel } from 'src/app/models/packResponseModel';
 
 @Component({
   selector: 'app-pack',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pack.component.css']
 })
 export class PackComponent implements OnInit {
+  packs: Pack[] = [];
+  apiUrl = 'https://hardworder.herokuapp.com/api/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     console.log("pack çalıştı");
+    this.getPacks();
+  }
+
+  getPacks() {
+    this.httpClient
+    .get<PackResponseModel>(this.apiUrl + 'pack/getAllUsersPacks')
+    .subscribe(response => { 
+      this.packs = response.data;
+    })
   }
 }
